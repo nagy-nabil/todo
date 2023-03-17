2416759040f6feb20e02340f47d0b25e203c0040
nagy nabil <nagynabil651@gmail.com>
2023-03-17 20:01:56 +0200

    react i hate it

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -44,23 +44,23 @@ app
  * Install Vue.js or any other extension in development mode only.
  * Note: You must install `electron-devtools-installer` manually
  */
-// if (import.meta.env.DEV) {
-//   app
-//     .whenReady()
-//     .then(() => import('electron-devtools-installer'))
-//     .then(module => {
-//       const {default: installExtension, VUEJS3_DEVTOOLS} =
-//         // @ts-expect-error Hotfix for https://github.com/cawa-93/vite-electron-builder/issues/915
-//         typeof module.default === 'function' ? module : (module.default as typeof module);
-//
-//       return installExtension(VUEJS3_DEVTOOLS, {
-//         loadExtensionOptions: {
-//           allowFileAccess: true,
-//         },
-//       });
-//     })
-//     .catch(e => console.error('Failed install extension:', e));
-// }
+if (import.meta.env.DEV) {
+  app
+    .whenReady()
+    .then(() => import('electron-devtools-installer'))
+    .then(module => {
+      const {default: installExtension, REACT_DEVELOPER_TOOLS} =
+        // @ts-expect-error Hotfix for https://github.com/cawa-93/vite-electron-builder/issues/915
+        typeof module.default === 'function' ? module : (module.default as typeof module);
+
+      return installExtension(REACT_DEVELOPER_TOOLS, {
+        loadExtensionOptions: {
+          allowFileAccess: true,
+        },
+      });
+    })
+    .catch(e => console.error('Failed install extension:', e));
+}
 
 /**
  * Check for app updates, install it in background and notify user that new version was installed.
